5a40d757919ffba9b29a0236bac390c9afe769e9
ellizza78 <elizoyinda06@gmail.com>
2025-07-06 23:31:35 +0100

    Create config.py
    Add config.py with tester settings

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+
+BINANCE_API_KEY = os.getenv("BINANCE_API_KEY")
+BINANCE_SECRET_KEY = os.getenv("BINANCE_SECRET_KEY")
+TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
+
+# Use Binance Futures TESTNET to bypass location restrictions
+BINANCE_TESTNET = True
